bd56a19558ce553f99eb1dca2178fa6b44b3f6dd
devanshu-sync <devanshu@syncmedia.io>
2025-07-29 18:16:09 +0530

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,6 +1,6 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-model_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
+model_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
 local_path = "./deepseek_model"
 
 # Download and save locally
